eb57b91e2296be14f8ccb321c7469775ab66315c
Ilya Zorin <by.92@mail.ru>
2024-12-22 07:53:58 +0500

    Initial project setup

---
+++ BluetoothScanner/App/BluetoothScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothScannerApp.swift
+//  BluetoothScanner
+//
+//  Created by Илья Зорин on 22.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScanView()
+        }
+    }
+}
---
+++ BluetoothScanner/Views/MainScanView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BluetoothScanner
+//
+//  Created by Илья Зорин on 22.12.2024.
+//
+
+import SwiftUI
+
+struct MainScanView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainScanView()
+}
